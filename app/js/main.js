65bdaafbfb096937f596777debb7265739518adc
jaredwhalen <jared.m.whalen@gmail.com>
2021-05-24 08:35:05 -0400

    Deployment

--- app/js/main.js
+++ app/js/main.js
@@ -20,7 +20,7 @@ function init() {
       Stepper.dimensions = setDimensions(Stepper)
       makeCard(Stepper)
       makeGraphics(Stepper)
-      Stepper.index = 9;
+      Stepper.index = 0;
       setActive(Stepper)
       Stepper.count = document.querySelectorAll(".slide").length
       progressBar(Stepper.index, 0)
